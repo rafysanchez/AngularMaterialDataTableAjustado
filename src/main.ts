27de86761302a6086800f05b18470c0a2396ef82
rafysanchez <rafysanchez@hotmail.com>
2018-11-21 17:37:30 -0200

    adjs

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './polyfills';
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './AppModule';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
